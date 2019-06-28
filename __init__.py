7208bd7a6268190a30c8509afa7b9536afa26ef1
Christopher <christopher.rocks@bell.net>
2019-06-28 11:16:03 -0400

    Built On init.py

--- __init__.py
+++ __init__.py
@@ -4,3 +4,8 @@ from SideScroller.Engine import *
 from SideScroller.Errors import *
 from SideScroller.SideScroller import *
 from SideScroller.utils import *
+
+from SideScroller.DisplayMethods.Color import Color
+from SideScroller.DisplayMethods.DisplayBase import DisplayBase
+from SideScroller.DisplayMethods.Image import Image
+from SideScroller.DisplayMethods.SpriteSheet import SpriteSheet

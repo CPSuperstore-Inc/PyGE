f04d44483872e85accffa42d47746c55104d93bc
Christopher <christopher.rocks@bell.net>
2019-06-28 12:52:20 -0400

    =Added Imports To init.py

--- __init__.py
+++ __init__.py
@@ -14,3 +14,18 @@ from SideScroller.Globals.Cache import *
 from SideScroller.Globals.Constants import *
 from SideScroller.Globals.GlobalVariable import *
 from SideScroller.Globals.Objects import *
+
+from SideScroller.Misc.AlarmClock import *
+from SideScroller.Misc.Computer import *
+from SideScroller.Misc.Font import *
+from SideScroller.Misc.Function import *
+from SideScroller.Misc.Queue import *
+from SideScroller.Misc.SpriteSheet import *
+from SideScroller.Misc.Ticker import *
+
+from SideScroller.Objects.Cube import *
+from SideScroller.Objects.ObjectBase import *
+from SideScroller.Objects.Text import *
+
+from SideScroller.Screens.Room import *
+from SideScroller.Screens.ScreenBase import *

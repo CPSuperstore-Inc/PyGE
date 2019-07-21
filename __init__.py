be8a6884e7f29cb41758cf568f831a53ebd869e5
Christopher <christopher.rocks@bell.net>
2019-07-21 10:47:39 -0400

    Added imports to __init__.py - Now simply import PyGE to gain access to everything

--- __init__.py
+++ __init__.py
@@ -1,32 +1,34 @@
-# TODO: This doesn't work for some reason...
-# from PyGE.ASQL import *
-# from PyGE.Audio import *
-# from PyGE.Engine import *
-# from PyGE.Errors import *
-# from PyGE.PyGE import *
-# from PyGE.utils import *
-# 
-# from PyGE.DisplayMethods.Color import *
-# from PyGE.DisplayMethods.DisplayBase import *
-# from PyGE.DisplayMethods.Image import *
-# from PyGE.DisplayMethods.SpriteSheet import *
-# 
-# from PyGE.Globals.Cache import *
-# from PyGE.Globals.Constants import *
-# from PyGE.Globals.GlobalVariable import *
-# from PyGE.Globals.Objects import *
-# 
-# from PyGE.Misc.AlarmClock import *
-# from PyGE.Misc.Computer import *
-# from PyGE.Misc.Font import *
-# from PyGE.Misc.Function import *
-# from PyGE.Misc.Queue import *
-# from PyGE.Misc.SpriteSheet import *
-# from PyGE.Misc.Ticker import *
-# 
-# from PyGE.Objects.Cube import *
-# from PyGE.Objects.ObjectBase import ObjectBase
-# from PyGE.Objects.Text import *
-# 
-# from PyGE.Screens.Room import *
-# from PyGE.Screens.ScreenBase import *
+from PyGE.ASQL import *
+from PyGE.Audio import *
+from PyGE.Engine import *
+from PyGE.Errors import *
+from PyGE.PyGE import *
+from PyGE.utils import *
+
+from PyGE.DisplayMethods.Color import *
+from PyGE.DisplayMethods.DisplayBase import *
+from PyGE.DisplayMethods.Image import *
+from PyGE.DisplayMethods.SpriteSheet import *
+
+from PyGE.Globals.Cache import *
+from PyGE.Globals.Constants import *
+from PyGE.Globals.GlobalVariable import *
+from PyGE.Globals.Objects import *
+
+from PyGE.Misc.AlarmClock import *
+from PyGE.Misc.Computer import *
+from PyGE.Misc.Font import *
+from PyGE.Misc.Function import *
+from PyGE.Misc.Queue import *
+from PyGE.Misc.SpriteSheet import *
+from PyGE.Misc.Ticker import *
+
+from PyGE.Objects.Cube import *
+from PyGE.Objects.ObjectBase import ObjectBase
+from PyGE.Objects.Text import *
+
+from PyGE.Screens.Room import *
+from PyGE.Screens.ScreenBase import *
+
+
+from pygame import Surface

def3d2a024ae49ce4877829314b678fd77e7a895
Christopher <christopher.rocks@bell.net>
2019-08-27 13:04:29 -0400

    Added SolidColor And Sound Objects

--- Globals/Objects.py
+++ Globals/Objects.py
@@ -1,7 +1,9 @@
 from PyGE.Objects.Cube import Cube
 from PyGE.Objects.Text import Text
+from PyGE.Objects.SolidColor import SolidColor
+from PyGE.Objects.Sound import Sound
 
 # The list of objects provided by the engine
 PREMADE_OBJECTS = [
-    Cube, Text
+    Cube, Text, SolidColor, Sound
 ]
